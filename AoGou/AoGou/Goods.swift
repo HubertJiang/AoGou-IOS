6262158650b829fab9251108bcaca364f49b1120
HubertJiang <xkjiang@hotmail.com>
2017-07-21 16:51:44 +0800

    initialize

---
+++ AoGou/AoGou/Goods.swift
@@ -0,0 +1,13 @@
+//
+//  Goods.swift
+//  AoGou
+//
+//  Created by liweihui on 2017/7/21.
+//  Copyright © 2017年 ios. All rights reserved.
+//
+
+import Foundation
+struct Goods{
+    var name:String
+    var content:String
+}
---
+++ AoGou/AoGou/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  AoGou
+//
+//  Created by liweihui on 2017/7/21.
+//  Copyright © 2017年 ios. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
